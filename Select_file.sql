eccece4cc7e9ea134df87c89e4538194b17d1b32
Evgeniy Bogoslavskiy <bogoslavskiy.ev@gmail.com>
2022-12-15 22:50:29 +0300

    update homework

--- Select_file.sql
+++ Select_file.sql
@@ -1,3 +1,5 @@
+--1 количество исполнителей в каждом жанре
+
   SELECT g.name, COUNT(e.id)
     FROM executor e
     JOIN genre_executor ge ON e.id = ge.executor_id
@@ -5,10 +7,14 @@
 GROUP BY g.name
 ORDER BY COUNT;
 
+--2 количество треков, вошедших в альбомы 2019-2020 годов
+
 SELECT COUNT(*)
   FROM track t
   JOIN album a ON t.album_id = a.id
  WHERE release_date IN (2019, 2020);
+
+-- 3 средняя продолжительность треков по каждому альбому
 
    SELECT a.name, AVG(duration)
      FROM track t
@@ -16,11 +22,18 @@ SELECT COUNT(*)
  GROUP BY a.name
  ORDER BY AVG;
 
-SELECT DISTINCT e.name
-  FROM executor e
-  JOIN executor_album ea ON e.id = ea.executor_id
-  JOIN album a ON ea.album_id = a.id
- WHERE release_date <> 2020;
+--4 все исполнители, которые не выпустили альбомы в 2020 году
+
+SELECT DISTINCT name
+  FROM executor
+ WHERE name NOT IN (
+        SELECT DISTINCT e.name
+        FROM executor e
+        JOIN executor_album ea ON e.id = ea.executor_id
+        JOIN album a ON ea.album_id = a.id
+        WHERE release_date = 2020);
+
+--5 названия сборников, в которых присутствует конкретный исполнитель (выберите сами)
 
 SELECT DISTINCT c.name
   FROM executor e
@@ -31,19 +44,26 @@ SELECT DISTINCT c.name
   JOIN compilation c ON ct.compilation_id = c.id
  WHERE e.name LIKE 'Eminem';
 
+--6 название альбомов, в которых присутствуют исполнители более 1 жанра
+
 SELECT DISTINCT a.name
-  FROM genre g
-  JOIN genre_executor ge ON g.id = ge.genre_id
-  JOIN executor e ON ge.executor_id = e.id
-  JOIN executor_album ea ON e.id = ea.executor_id
-  JOIN album a ON ea.album_id = a.id
- WHERE g.id > 1;
+    FROM genre g
+    JOIN genre_executor ge ON g.id = ge.genre_id
+    JOIN executor e ON ge.executor_id = e.id
+    JOIN executor_album ea ON e.id = ea.executor_id
+    JOIN album a ON ea.album_id = a.id
+GROUP BY a.name
+  HAVING COUNT (e.name)>1;
+
+--7 наименование треков, которые не входят в сборники
 
    SELECT t.name
      FROM track t
 FULL JOIN compilation_track ct ON t.id = ct.track_id
 FULL JOIN compilation c ON ct.compilation_id = c.id
-    WHERE c.id IS NULL ;
+    WHERE c.id IS NULL;
+
+--8 исполнителя(-ей), написавшего самый короткий по продолжительности трек (теоретически таких треков может быть несколько)
 
    SELECT e.name
      FROM executor e
@@ -54,7 +74,8 @@ FULL JOIN album a ON ea.album_id = a.id
  		  (SELECT MIN(duration)
  	        FROM track);
 
--- 1 вариант
+--9 название альбомов, содержащих наименьшее количество треков
+
   SELECT a.name
     FROM album a
     JOIN track t ON a.id = t.album_id
@@ -66,24 +87,6 @@ GROUP BY a.name
 ORDER BY COUNT
    LIMIT 1);
 
--- 2 вариант
-SELECT name
-  FROM album
- WHERE name IN
-  	(SELECT a.name
-       FROM album a
-       JOIN track t ON a.id = t.album_id
-   GROUP BY a.name
-   ORDER BY COUNT(t.name)
-      LIMIT 1);
-
--- 3 вариант
-  SELECT a.name
-    FROM album a
-    JOIN track t ON a.id = t.album_id
-GROUP BY a.name
-ORDER BY COUNT(t.name)
-   LIMIT 1;
 
 
 
